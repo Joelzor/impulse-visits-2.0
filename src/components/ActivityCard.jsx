6fc6fdf1ed52554906093cd99a1ca8b80b7e6a3a
Joel Watkins <eck087@yahoo.co.uk>
2023-03-25 17:00:42 +0000

    slight styling added to cards, add to plans button added

--- src/components/ActivityCard.jsx
+++ src/components/ActivityCard.jsx
@@ -1,3 +1,5 @@
+import { AiOutlinePlusCircle } from "react-icons/ai";
+
 const ActivityCard = ({ activity }) => {
   const { kinds, name, xid } = activity;
   // Splitting string into an array then replacing underscores with space
@@ -7,16 +9,22 @@ const ActivityCard = ({ activity }) => {
   });
 
   return (
-    <li className="mb-4 p-2 cursor-pointer rounded-lg w-[415px]  hover:bg-[#e1e0e0]">
-      <div>
+    <div className="flex gap-4">
+      <li className=" mb-6 p-2 cursor-pointer rounded-lg w-[415px] border bg-gray-100 hover:bg-[#e1e0e0]">
         <h3>{`${name.substring(0, 50)}...`}</h3>
         <p className="mt-2.5 italic">
           <span className="tag">{tagsFixed[0]}</span>{" "}
           <span className="tag">{tagsFixed[1]}</span>{" "}
           <span className="tag">{tagsFixed[2]}</span>
         </p>
-      </div>
-    </li>
+      </li>
+      <button
+        className="self-center rounded-full hover:bg-[#92ddc7] mb-6"
+        title="Add to plans!"
+      >
+        <AiOutlinePlusCircle className="h-6 w-auto text-[#43c59e] hover:text-black" />
+      </button>
+    </div>
   );
 };
 
