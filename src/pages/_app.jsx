c000cd3296a058f22de06995283263979206ce05
Joel Watkins <eck087@yahoo.co.uk>
2023-03-27 11:29:09 +0100

    auth functions set up in context file

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,13 +1,16 @@
 import "../styles/globals.css";
 import Layout from "../components/Layout";
 import { SearchProvider } from "../context/search";
+import { AuthProvider } from "../context/auth";
 
 export default function App({ Component, pageProps }) {
   return (
-    <SearchProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </SearchProvider>
+    <AuthProvider>
+      <SearchProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </SearchProvider>
+    </AuthProvider>
   );
 }
